7f9376b1b858ef366f1a7ff6e2a3372106a3ff01
Joanthan <poliekos@outlook.com>
2023-08-26 18:48:08 -0300

    now ok

--- database.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128))
-    body = db.Column(db.String(512))
-    # ... quaisquer outras colunas necessárias
