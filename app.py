535c995f6e9a1eeb01dfc734335f587c8a976c8a
Joanthan <poliekos@outlook.com>
2023-08-27 12:20:56 -0300

    tela de login, separadores e autores nos posts

--- app.py
+++ app.py
@@ -1,36 +1,82 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ.get('SECRET_KEY', 'mysecretkey')
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String)
+    posts = db.relationship('Post', back_populates='author', lazy=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', back_populates='posts')
 
-@app.route('/')
-def index():
-    posts = Post.query.all()
-    return render_template('index.html', posts=posts)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect('/')
+    return render_template('register.html')
 
-@app.route('/new', methods=['GET', 'POST'])
-def new():
+@app.route('/feed', methods=['GET', 'POST'])
+@login_required
+def feed():
     if request.method == 'POST':
         body = request.form['body']
-
-        # Add your validation logic here
-        # ...
-
-        post = Post(body=body)
-        db.session.add(post)
+        new_post = Post(body=body, user_id=current_user.id)
+        db.session.add(new_post)
         db.session.commit()
+        return redirect('/feed')
+    posts = Post.query.all()
+    return render_template('feed.html', posts=posts)
 
-        return redirect('/')
-    else:
-        return render_template('new.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/feed')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/feed')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
+
+@app.route('/')
+def home():
+    return redirect('/feed' if current_user.is_authenticated else '/login')
 
 if __name__ == "__main__":
     with app.app_context():
